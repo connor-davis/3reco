3d5ec5b9f318c88583bfcb664d4e11776ca5d7d9
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 21:41:59 +0200

    Add .well-known

--- api/index.js
+++ api/index.js
@@ -112,6 +112,7 @@ client.then(async () => {
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
   app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/.well-known'));
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
